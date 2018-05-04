260082c0702a5eee01cdf95de59218ca4bfd8030
Heath Burton <rubickdev@gmail.com>
2018-05-04 17:05:55 +1000

    presentation complete prior to calc functionality logic

---
+++ app.js
@@ -0,0 +1,9 @@
+var calcArray = [];
+
+// Click on buttons will make that number appear in the display area and store it in memory for later calculation
+
+// Hit a button to add an operation to the equation
+
+// Hit equals to calculate and display the calculator.
+
+// Hit clear to reset the calculator
