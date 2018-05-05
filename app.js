1fe2e0779f7a20d1243b3729f58dbe9847759a4e
Heath Burton <rubickdev@gmail.com>
2018-05-05 11:45:38 +1000

    1st JS update, eventlistener 1 working

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// Variables
 var calcArray = [];
+var display = 0;
+var buttons = document.querySelector('#wrapper');
+
 
 // Click on buttons will make that number appear in the display area and store it in memory for later calculation
+buttons.addEventListener("click", function(e){
+  
+  console.log("registering " + e.target.value);
+  e.stopPropagation();
+});
 
 // Hit a button to add an operation to the equation
 
