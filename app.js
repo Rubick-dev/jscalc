c5ff9d64e464025fa0236a72e8f1845b5335f0c1
Heath Burton <rubickdev@gmail.com>
2018-05-06 13:51:47 +1000

    JS logic 90% done, just a few bugs to iron out

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Variables
-var calcArray = [];
-var val = -1;
-var val2 = 0;
-var display = 0;
-var display2;
-var buttons = document.querySelector('#wrapper');
-var prevOp;
+var calcArray = []; //Array used to store the numerals pressed on the calc
+var val = null; // the value of the current total
+var val2 = 0; // the value of the user defined value to be calculated with val
+var display = 0; // to display the main calc value
+var display2; //
+var buttons = document.querySelector('#wrapper'); //store the clcik event variable
+var prevOp; // to remember the previous op for calculation on chained calcs
 
 // Event Listener to trigger a switch statement and fire the appropriate function logic.
 buttons.addEventListener("click", calculate, false);
@@ -16,10 +16,7 @@ function calculate(e){
   
     switch (testval) {
       case "clear":
-        display = 0;
-        calcArray = [];
-        val = -1;
-        resetDisplay(display);
+        resetDisplay();
         console.log("registered pressing the " + testval + " button");
         break;
   
@@ -51,6 +48,7 @@ function calculate(e){
         break;
 
       case "=":
+        calculateResult(testval);
         console.log("registered pressing a " + testval);
         break;
     }
@@ -60,9 +58,13 @@ function calculate(e){
 
 
 // Clear Button Function
-function resetDisplay(clear) {
-  document.getElementById("result").innerHTML = clear;
+function resetDisplay() {
+  display = 0;
+  calcArray = [];
+  val = null;
+  document.getElementById("result").innerHTML = display;
   document.getElementById("miniResult").innerHTML = "";
+  
 };
 
 
@@ -101,18 +103,24 @@ function insertPoint(point) {
   }
 };
 
-
+// Run Arithmatic logic upon arithmatic operatror button press
 function runFraction(op){
-  if (val < 0) {
+  if (val === null && calcArray.length > 0) {
     val = calcArray.join('');
     calcArray = [];
-    display = calcArray.join("");
+    display = 0;
     prevOp = op;
     document.getElementById("result").innerHTML = display;
-    document.getElementById("miniResult").innerHTML = val + " (" + op + ")";
-  } else if (calcArray.length == 0){
-    document.getElementById("miniResult").innerHTML = val + " (" + op + ")";
+    document.getElementById("miniResult").innerHTML = "(" + op + ") " + val;
+    console.log("A if statement");
+  } else if (val && calcArray.length == 0){
+    document.getElementById("miniResult").innerHTML = "(" + op + ") " + val;
+    display = 0;
     prevOp = op;
+    document.getElementById("result").innerHTML = display;
+    console.log("B if statement");
+  } else if (val === null && calcArray.length == 0) {
+    console.log("C if statement");
   } else {
     val2 = calcArray.join('');
     calcArray = [];
@@ -120,16 +128,22 @@ function runFraction(op){
     document.getElementById("result").innerHTML = display;
     val = conductArithmatic(prevOp);
     console.log("calculate now " + val + " " + op + " " + val2);
-    document.getElementById("miniResult").innerHTML = val + " (" + op + ")";
+    document.getElementById("miniResult").innerHTML = "(" + op + ") " + val;
     prevOp = op;
+    console.log("D if statement");
   }
 };
 
-// Carry out Arithmatic based on user entries
+// Carry out Arithmatic calculation based on user entries
 function conductArithmatic(op) {
   switch (op) {
     case "+":
-      return (+val + +val2);
+      let arithVal = (+val + +val2);
+      if (arithVal.toString().length > 12) {
+        displayError();
+        break;
+      } else 
+      return arithVal;
 
     case "/":
       return (+val / +val2);
@@ -139,8 +153,29 @@ function conductArithmatic(op) {
 
     case "-":
       return (+val - +val2);
-
   }
 };
 
+function calculateResult(op) {
+  if (val && calcArray.length > 0) {
+    val2 = calcArray.join('');
+    display = conductArithmatic(prevOp);
+    document.getElementById("result").innerHTML = display;
 
+    calcArray = [];
+    val = display;
+    document.getElementById("miniResult").innerHTML = "";
+    console.log("= sign statement logic ha just run");
+  }
+}
+
+
+function displayError(){
+  display = "Calc Broke!";
+  document.getElementById("result").innerHTML = display;
+  document.getElementById("miniResult").innerHTML = "";
+  calcArray = [];
+  val = null;
+  val2 = 0;
+  display = 0;
+}
